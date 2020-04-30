ec3bbc54d6901bb7202dae747b17eba2f8f25d38
Will Roberts <robertsw.tx@gmail.com>
2020-04-30 19:54:27 -0400

    Startup
    Basic files added.

---
+++ config/config.php
@@ -0,0 +1,6 @@
+<?php
+  $title = "Title";
+  $slogan = "Slogan";
+
+
+?>
---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+  require_once('config/config.php');
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title><?php echo $title; ?></title>
+    <link rel="stylesheet" href="">
+    <script src=""></script>
+  </head>
+  <body>
+    <h1><?php echo $title ?></h1>
+  </body>
+</html>

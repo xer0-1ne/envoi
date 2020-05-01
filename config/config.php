6bf4022053a0b68c6959d3d0413689c01525e766
Will Roberts <robertsw.tx@gmail.com>
2020-04-30 22:18:20 -0400

    Basic layout design.
    Created the basic layout design to get the look/feel of the site.

--- config/config.php
+++ config/config.php
@@ -1,6 +1,11 @@
 <?php
-  $title = "Title";
-  $slogan = "Slogan";
+  $title = 'Skudal'; //Name of the blog
+  $slogan = 'One Place to Share it All'; //slogan
+  $site_url = 'localhost'; //link to website
 
+  $language = "en"; //use HTML head approved language tags
+
+  $seo_description = ''; //description for SEO
+  $author = 'Awesome Guy'; //Your name
 
 ?>
